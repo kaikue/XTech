0da888cde86662fcac513e81142cf559bd70f0dc
Kai Kuehner <kvk@pobox.com>
2016-10-24 01:41:56 -0400

    Fix inserter bounding boxes

--- src/main/java/kaikue/xtech/blocks/BlockFluidInserter.java
+++ src/main/java/kaikue/xtech/blocks/BlockFluidInserter.java
@@ -3,54 +3,19 @@ package kaikue.xtech.blocks;
 import kaikue.xtech.tileentities.TileEntityFluidInserter;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockFluidInserter extends DirectionalBaseBlock implements ITileEntityProvider {
-
-	private static final AxisAlignedBB INSERTER_AABB = new AxisAlignedBB(0.125D, 0.0D, 0.125D, 0.875D, 1.0D, 0.875D);
+public class BlockFluidInserter extends BlockInserter implements ITileEntityProvider {
 
 	public BlockFluidInserter() {
 		super("fluidinserter", "Transfers fluids into remote tanks", Material.IRON);
 	}
 
-	@Override
-	public boolean isOpaqueCube(IBlockState state) {
-		return false;
-	}
-
-	@Override
-	public boolean isFullCube(IBlockState state) {
-		return false;
-	}
-
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		//TODO directional AABBs
-		return INSERTER_AABB;
-	}
-
 	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
 		//TODO check for tank?
 		return super.canPlaceBlockAt(worldIn, pos);
-	}
-
-	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
-	{
-		return this.getDefaultState().withProperty(FACING, facing);
-	}
-
-	public IBlockState getStateFromMeta(int meta)
-	{
-		IBlockState iblockstate = this.getDefaultState();
-		iblockstate = iblockstate.withProperty(FACING, EnumFacing.getFront(meta));
-		return iblockstate;
 	}
 
 	@Override
---
+++ src/main/java/kaikue/xtech/blocks/BlockInserter.java
@@ -0,0 +1,60 @@
+package kaikue.xtech.blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public abstract class BlockInserter extends DirectionalBaseBlock {
+
+	private static final AxisAlignedBB INSERTER_UP_AABB = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.5D, 0.75D);
+	private static final AxisAlignedBB INSERTER_DOWN_AABB = new AxisAlignedBB(0.25D, 0.5D, 0.25D, 0.75D, 1.0D, 0.75D);
+	private static final AxisAlignedBB INSERTER_NORTH_AABB = new AxisAlignedBB(0.25D, 0.25D, 0.5D, 0.75D, 0.75D, 1.0D);
+	private static final AxisAlignedBB INSERTER_SOUTH_AABB = new AxisAlignedBB(0.25D, 0.25D, 0.0D, 0.75D, 0.75D, 0.5D);
+	private static final AxisAlignedBB INSERTER_EAST_AABB = new AxisAlignedBB(0.0D, 0.25D, 0.25D, 0.5D, 0.75D, 0.75D);
+	private static final AxisAlignedBB INSERTER_WEST_AABB = new AxisAlignedBB(0.5D, 0.25D, 0.25D, 1.0D, 0.75D, 0.75D);
+
+	public BlockInserter(String name, String description, Material material) {
+		super(name, description, material);
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		switch(state.getValue(FACING)) {
+		case UP: return INSERTER_UP_AABB;
+		case DOWN: return INSERTER_DOWN_AABB;
+		case NORTH: return INSERTER_NORTH_AABB;
+		case SOUTH: return INSERTER_SOUTH_AABB;
+		case EAST: return INSERTER_EAST_AABB;
+		case WEST: return INSERTER_WEST_AABB;
+		default: return INSERTER_UP_AABB;
+		}
+	}
+
+	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+	{
+		return this.getDefaultState().withProperty(FACING, facing);
+	}
+
+	public IBlockState getStateFromMeta(int meta)
+	{
+		IBlockState iblockstate = this.getDefaultState();
+		iblockstate = iblockstate.withProperty(FACING, EnumFacing.getFront(meta));
+		return iblockstate;
+	}
+
+}
