cc518bece48ddecef4e3a077fef8e787382f34d3
Kai Kuehner <kvk@pobox.com>
2016-10-23 23:10:18 -0400

    Increase fluid transfer rate

--- src/main/java/kaikue/xtech/Config.java
+++ src/main/java/kaikue/xtech/Config.java
@@ -7,18 +7,15 @@ import net.minecraftforge.common.config.Configuration;
 
 public class Config {
 	private static final String CATEGORY_GENERAL = "general";
-	//private static final String CATEGORY_OREGEN = "ore_generation";
 
 	public static int beamDistance = 64;
-	public static int fluidTransfer = 10;
-	//public static boolean genCopper = true;
+	public static int fluidTransfer = 100;
 
 	public static void readConfig() {
 		Configuration cfg = CommonProxy.config;
 		try {
 			cfg.load();
 			initGeneralConfig(cfg);
-			//initOregenConfig(cfg);
 		} catch (Exception ex) {
 			XTech.logger.log(Level.ERROR, "Problem loading config file!", ex);
 		} finally {
@@ -31,12 +28,6 @@ public class Config {
 	private static void initGeneralConfig(Configuration cfg) {
 		cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
 		beamDistance = cfg.getInt("beamDistance", CATEGORY_GENERAL, beamDistance, 1, 256, "Maximum distance beams can travel");
-		fluidTransfer = cfg.getInt("fluidTransfer", CATEGORY_GENERAL, fluidTransfer, 1, 100000, "Amount of fluid (in mB) to transfer");
+		fluidTransfer = cfg.getInt("fluidTransfer", CATEGORY_GENERAL, fluidTransfer, 1, 100000, "Amount of fluid (in mB) to transfer per half-second");
 	}
-
-	/*private static void initOregenConfig(Configuration cfg) {
-		cfg.addCustomCategoryComment(CATEGORY_OREGEN, "Ore generation configuration");
-		String oreComment = "Whether to generate ";
-		genCopper = cfg.getBoolean("copper", CATEGORY_OREGEN, genCopper, oreComment + "copper ore");
-	}*/
 }
