74e17772d51c37136ba4d27597be402ac4444d1a
Kai Kuehner <kvk@pobox.com>
2016-11-04 22:10:21 -0400

    Improve splitter model

--- src/main/java/kaikue/xtech/blocks/BlockSplitter.java
+++ src/main/java/kaikue/xtech/blocks/BlockSplitter.java
@@ -2,11 +2,20 @@ package kaikue.xtech.blocks;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockSplitter extends BlockMirror {
 
 	public BlockSplitter() {
 		super("splitter", "Splits beams in two", Material.GLASS, 0.3f, SoundType.GLASS);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
 	}
 
 }
